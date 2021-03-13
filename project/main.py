c593f2747ba3de5e39ee100e0c26275334e2ecdd
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 20:59:31 -0300

    heroku testes

--- project/main.py
+++ project/main.py
@@ -13,9 +13,9 @@ def profile():
   return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
 
 if __name__ == "__main__":
-app = create_app()
-db.create_all(app)
+  app = create_app()
+  db.create_all(app)
 
-app.register_blueprint(main_blueprint)
-main = Blueprint('main', __name__)
-app.run()
+  app.register_blueprint(main_blueprint)
+  main = Blueprint('main', __name__)
+  app.run()

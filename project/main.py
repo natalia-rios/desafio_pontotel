9af8dcae2e951a1a2df109070ee7112328b02c1a
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 19:10:27 -0300

    heroku testes

--- project/main.py
+++ project/main.py
@@ -11,5 +11,5 @@ def index():
 def profile():
   return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
 
-if __name__ == "__main__":
+def main():
   main.run(debug=True) 

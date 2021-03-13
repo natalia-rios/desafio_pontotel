622d7bc5d449fdfd3fb303988187dec4351c7681
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 19:58:08 -0300

    heroku testes

--- project/__init__.py
+++ project/__init__.py
@@ -48,7 +48,7 @@ def create_app():
   app.register_blueprint(google_blueprint, url_prefix = "/login")
 
   #non-auth parts
-  import .main as main_blueprint
+  main = Blueprint('main', __name__)
   app.register_blueprint(main_blueprint)
 
   return app

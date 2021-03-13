92ae0a725bd0a072bc1a0211bf2f21f4ec733817
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 19:28:47 -0300

    heroku testes

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template 
 from flask_login import login_required, current_user
 from . import db 
+from project import app
+from project import db, create_app
 
 main = Blueprint('main', __name__)
 
@@ -13,5 +15,7 @@ def index():
 def profile():
   return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
 
-def main(localhost, port):
-  main.run(5000, '127.0.0.1') 
+db.create_all(app=create_app())
+
+if __name__ == "__main__":
+  app.run() 

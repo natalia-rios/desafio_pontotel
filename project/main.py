949747048781ffc560d008d64619b111221d4685
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 19:24:33 -0300

    heroku testes

--- project/main.py
+++ project/main.py
@@ -12,3 +12,6 @@ def index():
 @login_required
 def profile():
   return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
+
+def main(localhost, port):
+  main.run(5000, '127.0.0.1') 

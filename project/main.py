9c0b198f2194e372a4f068b795e12e77e0fa07ee
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 17:53:26 -0300

    limpando db

--- project/main.py
+++ project/main.py
@@ -12,6 +12,3 @@ def index():
 @login_required
 def profile():
   return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
-
-if __name__ == '__main__':
-    app.run(threaded=True, port=5000)

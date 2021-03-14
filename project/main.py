cb5258a9bb372042bf46e1a1403ef49f914fe45c
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 21:20:09 -0300

    heroku testes

--- project/main.py
+++ project/main.py
@@ -15,9 +15,7 @@ def index():
 def profile():
   return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
 
-
-def main(port, server):
-
+if '__main__':
   app = create_app()
   with app.app_context():
     db.create_all()

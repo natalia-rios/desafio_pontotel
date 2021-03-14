238707cc6142bb6a353d8ed4ce688da0e522ed0b
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 21:08:18 -0300

    heroku testes

--- project/main.py
+++ project/main.py
@@ -3,21 +3,23 @@ from flask_login import login_required, current_user
 from project import create_app, db
 from flask import Flask
 
-def main(port, server):
-  @main.route('/')
-  def index():
-    return render_template('index.html')
+main = Blueprint('main', __name__)
 
-  @main.route('/profile')
-  @login_required
-  def profile():
-    return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
+@main.route('/')
+def index():
+  return render_template('index.html')
+
+@main.route('/profile')
+@login_required
+def profile():
+  return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
+
+
+def main(port, server):
 
   app = create_app()
   db.create_all(app)
-
-  app.register_blueprint(main_blueprint)
-  main = Blueprint('main', __name__)
+  app.register_blueprint(main)
   app.run(port, server, debug = True)
 
 

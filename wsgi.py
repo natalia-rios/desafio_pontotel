0259b9259595a8b0f22fe9614f79433c2535b029
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-14 15:29:43 -0300

    heroku testes

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from project.main import main
-  
-if __name__ == "__main__": 
-    main.run() 
+from project import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
+else:
+    app = create_app()

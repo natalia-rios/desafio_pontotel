95a03d68de4ced85d20b98e5115b1b925cb76656
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-13 21:02:01 -0300

    heroku testes

--- project/main.py
+++ project/main.py
@@ -3,16 +3,17 @@ from flask_login import login_required, current_user
 from project import create_app, db
 from flask import Flask
 
-@main.route('/')
-def index():
-  return render_template('index.html')
-
-@main.route('/profile')
-@login_required
-def profile():
-  return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
-
 if __name__ == "__main__":
+  
+  @main.route('/')
+  def index():
+    return render_template('index.html')
+
+  @main.route('/profile')
+  @login_required
+  def profile():
+    return render_template('profile.html', username = current_user.username, email = current_user.email, password = current_user.password, country = current_user.country, state = current_user.state, city = current_user.city, CEP = current_user.CEP, rua = current_user.rua, numero = current_user.numero, complemento = current_user.complemento, CPF = current_user.CPF, PIS = current_user.PIS)
+
   app = create_app()
   db.create_all(app)
 

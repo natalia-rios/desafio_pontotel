510a5ce21a6bf855078c566067b6146a99774377
natalia-rios <nataliarios@projetos.ifce.edu.br>
2021-03-16 20:50:07 -0300

    atualizando login com github

--- wsgi.py
+++ wsgi.py
@@ -2,10 +2,7 @@ from project.main import main
 from project import create_app, db
 import os
 
-os.remove('project\db.sqlite')
-
 if __name__ == '__main__':
-    db.create_all(app=create_app())
     app = create_app()
     app.run()
 else:
